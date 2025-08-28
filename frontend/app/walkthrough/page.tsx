97d00433765cb995146f79ca095dd99c8fccb61f
André Abadesso <andre.abadesso@gmail.com>
2025-08-28 17:58:38 -0300

    feat(walkthrough): add deployment screen

---
+++ frontend/app/walkthrough/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Walkthrough } from '@/components/Walkthrough/Walkthrough';
+
+export default function WalkthroughPage() {
+  return (
+    <div className="h-full">
+      <Walkthrough />
+    </div>
+  );
+}
