661bf893035343fb4824ac5415bcee395e7a4037
André Abadesso <andre.abadesso@gmail.com>
2025-11-28 14:46:28 -0300

    feat(frontend): added google analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter, Satisfy } from 'next/font/google'
 import { Providers } from './providers'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 const satisfy = Satisfy({ subsets: ['latin'], weight: '400', variable: '--font-satisfy' })
@@ -17,6 +18,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-289QLXBN2M"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-289QLXBN2M');
+          `}
+        </Script>
+      </head>
       <body className={`${inter.className} ${satisfy.variable} h-full bg-gray-50`}>
         <Providers>
           {children}
