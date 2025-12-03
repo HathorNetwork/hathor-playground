869e549908f21bcc4724c34b62be4f1bd769b204
André Abadesso <andre.abadesso@gmail.com>
2025-12-03 17:44:42 -0300

    feat: added twitter pixel (#42)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -32,6 +32,16 @@ export default function RootLayout({
             gtag('config', 'G-289QLXBN2M');
           `}
         </Script>
+        
+        {/* X conversion tracking base code */}
+        <Script id="x-conversion-tracking" strategy="afterInteractive">
+          {`
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            twq('config','o58pe');
+          `}
+        </Script>
       </head>
       <body className={`${inter.className} ${satisfy.variable} h-full bg-gray-50`}>
         <Providers>
